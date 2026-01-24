e11bb8db0a0e362d9cf8c5c8b3baf76644778b5b
H82M19\Student <Student>
2026-01-24 16:22:39 +0700

    first commit

---
+++ week1/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+
+    return (
+        <>
+            <hr />
+        </>
+    )
+}
+
+export default Footer;
---
+++ week1/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+function Header(){
+
+    return (
+        <>
+            <h3>
+                Quản Lý Sinh Viên
+            </h3>
+        </>
+    )
+}
+
+export default Header;
